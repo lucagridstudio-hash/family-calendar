deec3e19caec17b502f90d44616914a73059b8da
lucagridstudio-hash <luca.gridstudio@gmail.com>
2026-09-17 18:06:04 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/family-calendar/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
